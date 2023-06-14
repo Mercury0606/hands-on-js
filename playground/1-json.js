321289c8cebebafb139ade08a915f06f6fcc4fdb
Mercury0606 <mercuryzhong0606@gmail.com>
2023-06-14 23:59:21 +0800

    2023.-6.13 Commit

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,7 +1,25 @@
-const book={
-    title:   'Ego is the Enemy',
-    author:  'Ryan Holiday'
-}
- console.log(JSON.stringify(book));
-console.log(book);
-console.log(book.title);
+const fs = require('fs');
+const {parse} = require("nodemon/lib/cli");
+
+
+// const book={
+//     title:   'Ego is the Enemy',
+//     author:  'Ryan Holiday'
+// }
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json',bookJSON)
+//
+// const dataBuffer = fs.readFileSync('1-json.json');
+// // console.log(dataBuffer.toString());
+// const parseData = JSON.parse(dataBuffer);
+// console.log(parseData.author);
+
+
+const buffer = fs.readFileSync('1-json.json');
+const bufferString = buffer.toString();
+const parseString = JSON.parse(bufferString);
+parseString.name='changed Name'
+parseString.age='22'
+
+const stringifyData = JSON.stringify(parseString);
+fs.writeFileSync('1-json.json',stringifyData)
