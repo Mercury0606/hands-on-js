1a0c50b43aeb2dcf65108923e442d1ee7fc40364
chongyuan.zhong <chongyuna.a.zhong@rakuten.com>
2023-06-29 17:59:27 +0800

    2023.06.29 1st commit

--- weather_app/hello.js
+++ weather_app/hello.js
@@ -1,11 +1,38 @@
 const request= require('request')
-const url='http://api.weatherstack.com/current?access_key=a82b885dc5ebfe71ba4dc68c84fc0365&query=Dalian&units=f'
-request({url:url,json:true},(error,response)=>{
-   const data = response.body;
-   const current = data.current;
-  //   console.log(data);
-  // console.log(data.current);
-    console.log(current.weather_descriptions[0]+" It is currently " + current.temperature + " degree out. there is a "+current.precip+"% chance of rain");
-})
+const url='http://api.weatherstack.com/current?access_key=a82b885dc5ebfe71ba4dc68c84fc0365&query=dalian'//unit=f
+// request({url:url,json:true},(error,response)=>{
+//    const data = response.body;
+//    const current = data.current;
+//
+//     if (error){
+//         console.log("Unable to connect to weather server");
+//         console.log(error.code, error.error);
+//     }
+//     else if (response.body.error){
+//         console.log("Unable to find location");
+//     }
+//     else {
+//         console.log(current.weather_descriptions[0]+" It is currently " + current.temperature + " degree out. there is a "+current.precip+"% chance of rain");
+//
+//     }
+// })
+
+
 //Geocoding
-weatherURL='https://api.mapbox.com/geocoding/v5/mapbox.places/San%20Francisco.json?access_token=pk.eyJ1IjoibWVyYzRydnJ0IiwiYSI6ImNsamNmZmJyNTEyNzEzY3BlaWZ2aXJlcDkifQ.sFZgsIPBSnfxq0TPzC6GwQ'
+// weatherURL='https://api.mapbox.com/geocoding/v5/mapbox.places/San%20Francisco.json?access_token=pk.eyJ1IjoibWVyYzRydnJ0IiwiYSI6ImNsamNmZmJyNTEyNzEzY3BlaWZ2aXJlcDkifQ.sFZgsIPBSnfxq0TPzC6GwQ'
+request({url:weatherURL},(err,response)=>{
+        if (error){
+        console.log("Unable to connect to weather server");
+        console.log(error.code, error.error);
+    }
+    else if (response.body.error){
+        console.log("Unable to find location");
+    }
+    else {
+        console.log(current.weather_descriptions[0]+" It is currently " + current.temperature + " degree out. there is a "+current.precip+"% chance of rain");
+
+    }
+     const latitude =response.body.features[0].center[1]
+     const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude);
+})
